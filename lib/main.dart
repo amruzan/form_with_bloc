04d67e03e57b46840c54ef0043578b84d44e59c6
Amritha Anand <sandeepanand@Amrithas-MacBook-Air.local>
2022-12-02 16:09:46 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_forms2/register_page.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+        primarySwatch: Colors.blue,
+      ),
+      builder: (context, child) {
+        return FormThemeProvider(
+            theme: FormTheme(
+                checkboxTheme: CheckboxFieldTheme(canTapItemTile: true)),
+            child: child!);
+      },
+      home: AllFormFields(),
+    );
+  }
+}
+
+class FieldsFormBloc extends FormBloc<String, String> {
+  final nameText = TextFieldBloc(validators: [FieldBlocValidators.required]);
+  final pwdText = TextFieldBloc(validators: [FieldBlocValidators.required]);
+
+  FieldsFormBloc() : super(autoValidate: true) {
+    addFieldBlocs(fieldBlocs: [
+      nameText,
+      pwdText,
+    ]);
+  }
+
+  @override
+  void onSubmitting() async {
+    try {
+      emitSuccess(canSubmitAgain: true);
+    } catch (e) {
+      emitFailure(failureResponse: "Failed to Save");
+    }
+  }
+}
+
+class AllFormFields extends StatelessWidget {
+  const AllFormFields({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => FieldsFormBloc(),
+      child: Builder(builder: (context) {
+        final formBloc = BlocProvider.of<FieldsFormBloc>(context);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Login Page"),
+          ),
+          body: FormBlocListener<FieldsFormBloc, String, String>(
+              onSubmitting: (context, state) {
+                LoadingDialog.show(context);
+              },
+              onSubmissionFailed: (context, state) {
+                LoadingDialog.hide(context);
+              },
+              onFailure: (context, state) {
+                LoadingDialog.hide(context);
+              },
+              onSuccess: (context, state) {
+                LoadingDialog.hide(context);
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (_) => SuccessScreen(
+                          name: formBloc.nameText.value,
+                        )));
+              },
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20,),
+                      Text("Welcome",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 30,color: Colors.blue),),
+                      SizedBox(height: 20,),
+                      TextFieldBlocBuilder(
+                        textFieldBloc: formBloc.nameText,
+                        decoration: InputDecoration(
+                            labelText: "Enter Name",
+                            prefixIcon: Icon(Icons.person)),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFieldBlocBuilder(
+                        textFieldBloc: formBloc.pwdText,
+                        suffixButton: SuffixButton.obscureText,
+                        decoration: InputDecoration(
+                            labelText: "Enter Password",
+                            prefixIcon: Icon(Icons.lock)),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0)),
+                            )),
+                            onPressed: () {
+                              formBloc.submit();
+                            },
+                            child: Text("Sign In",style: TextStyle(fontSize: 15),)),
+                      ),
+                      SizedBox(height: 15,),
+                      Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text("Do not have an Account?",style: TextStyle(fontSize: 20,),),
+                      TextButton(onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>RegistrationPage()));
+                      }, child: Text("Sign Up",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),))],)
+                    ],
+                  ),
+                ),
+              )),
+        );
+      }),
+    );
+  }
+}
+
+class LoadingDialog extends StatelessWidget {
+  static void show(BuildContext context, {Key? key}) => showDialog<void>(
+        context: context,
+        useRootNavigator: false,
+        barrierDismissible: false,
+        builder: (_) => LoadingDialog(key: key),
+      ).then((_) => FocusScope.of(context).requestFocus(FocusNode()));
+
+  static void hide(BuildContext context) => Navigator.pop(context);
+
+  const LoadingDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Center(
+        child: Card(
+          child: Container(
+            width: 80,
+            height: 80,
+            padding: const EdgeInsets.all(12.0),
+            child: const CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SuccessScreen extends StatelessWidget {
+  SuccessScreen({Key? key, required this.name}) : super(key: key);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(Icons.tag_faces, size: 100),
+            const SizedBox(height: 10),
+            Text(
+              'Welcome $name',
+              style: TextStyle(fontSize: 54, color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(
+              onPressed: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => AllFormFields())),
+              icon: const Icon(Icons.logout),
+              label: const Text('Sign Out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register_page.dart
@@ -0,0 +1,181 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_forms2/main.dart';
+import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+
+class FormRegistrationBloc extends FormBloc<String, String> {
+  final testField = TextFieldBloc(validators:[FieldBlocValidators.required]);
+  final nameField = TextFieldBloc(validators: [requiredValues]);
+  final emailField = TextFieldBloc(
+      validators: [FieldBlocValidators.required, FieldBlocValidators.email]);
+  final dob = InputFieldBloc<DateTime?, Object>(initialValue: null);
+  final gender = SelectFieldBloc(
+      items: ['Male', 'Female'], validators: [FieldBlocValidators.required]);
+  final qualification = SelectFieldBloc(items: ['Degree', 'Masters', 'Others']);
+
+  static String? requiredValues(dynamic value) {
+    if (value == null ||
+        value == false ||
+        ((value is Iterable || value is String || value is Map) &&
+            value.length == 0)) {
+      return "Enter name";
+    }
+    return null;
+  }
+
+  FormRegistrationBloc() : super(autoValidate: true) {
+    addFieldBlocs(fieldBlocs: [
+      this.testField,
+      this.nameField,
+      this.emailField,
+      this.gender,
+      this.dob,
+      this.qualification,
+    ]);
+  }
+  void addErrors(){
+    testField.addFieldError("Test Field required");
+    nameField.addFieldError("Name required");
+    emailField.addFieldError("Email required");
+    gender.addFieldError("Gender required");
+    dob.addFieldError("D.O.B. required");
+    qualification.addFieldError("Qualification required");
+  }
+
+  @override
+  void onSubmitting() {
+    print("On Submit");
+    try {
+      print(nameField.value);
+      emitSuccess();
+
+    }
+    catch (e) {
+      print(e.toString());
+      emitFailure(failureResponse: e.toString());
+
+    }
+  }
+}
+
+class RegistrationPage extends StatelessWidget {
+  RegistrationPage({Key? key}) : super(key: key);
+  TextEditingController test = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => FormRegistrationBloc(),
+      child: Builder(builder: (context) {
+        final registrationBloc = BlocProvider.of<FormRegistrationBloc>(context);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Registration Page"),
+          ),
+          floatingActionButton: Container(child: FloatingActionButton.extended(onPressed: registrationBloc.addErrors,icon: Icon(Icons.error), label: Text("Required Fields")),),
+          body: FormBlocListener<FormRegistrationBloc, String, String>(
+            onSubmitting: (context, state) {
+              print("Submitting..");
+            },
+            onSubmissionFailed: (context,state){
+              print(registrationBloc.testField.value);
+              print("Error Occurred");
+            },
+            onSuccess: (context, state) {
+              Navigator.pop(context);
+            },
+            onFailure: (context, state) {
+              print(state.failureResponse);
+              final failResponse = state.failureResponse;
+            },
+            child: ScrollableFormBlocManager(
+              formBloc: registrationBloc,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      BlocBuilder<TextFieldBloc, TextFieldBlocState>(
+                        bloc: registrationBloc.testField,
+                        builder: (context, state) {
+                          return TextFormField(
+                            controller: test,
+                            onChanged: (val){
+                              registrationBloc.testField.updateValue(val);
+                            },
+                            decoration: InputDecoration(labelText: "Test Field",errorText: state.canShowError ? state.error.toString() : ""),);
+                        },
+                      ),
+                      SizedBox(height: 10,),
+                      TextFieldBlocBuilder(
+                        textFieldBloc: registrationBloc.nameField,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.person), labelText: "Name"),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFieldBlocBuilder(
+                        textFieldBloc: registrationBloc.emailField,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.mail), labelText: "Email"),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      DateTimeFieldBlocBuilder(
+                        dateTimeFieldBloc: registrationBloc.dob,
+                        format: DateFormat('dd/MM/yy'),
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2100),
+                        decoration: InputDecoration(
+                            labelText: 'Date of Birth',
+                            prefixIcon: Icon(Icons.calendar_month)
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      RadioButtonGroupFieldBlocBuilder<String>(
+                          selectFieldBloc: registrationBloc.gender,
+                          decoration: InputDecoration(
+                            labelText: "Gender",
+                          ),
+                          groupStyle: FlexGroupStyle(),
+                          itemBuilder: (context, item) =>
+                              FieldItem(child: Text(item))),
+                      SizedBox(height: 10,),
+                      DropdownFieldBlocBuilder<String>(
+                          selectFieldBloc: registrationBloc.qualification,
+                          decoration: InputDecoration(labelText: "Qualification",
+                              prefixIcon: Icon(Icons.book_outlined)),
+                          itemBuilder: (context, value) =>
+                              FieldItem(
+                                  child: Text(value))),
+                      SizedBox(height: 10,),
+                      Container(padding: EdgeInsets.all(5),
+                          width: MediaQuery
+                              .of(context)
+                              .size
+                              .width,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // registrationBloc.addErrors();
+                              registrationBloc.submit();
+                            }, child: Text(
+                            "Register", style: TextStyle(fontSize: 15),),
+                            style: ButtonStyle(shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)))),))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
